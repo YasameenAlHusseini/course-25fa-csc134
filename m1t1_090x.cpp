784efffa74bd5a78d5c3bd9c2146f3e0760faef1
Yasameen Al-Husseini <yasameenalhusseini@gmail.com>
2025-09-08 00:36:25 +0000

    COMMIT

--- m1t1_090x.cpp
+++ m1t1_090x.cpp
@@ -5,3 +5,20 @@ Yasameen A
 9/7/25
 Program will greet the user.
 */
+
+// magic words <3
+
+#include <iostream>
+using namespace std; 
+
+int main () {
+    // input: start the program
+    // processing: none
+    // output: 0 (no erors)
+    // special FX: Says "Hello" to the user.
+
+    // Let's say hello
+    cout << "Hello, user!" << endl; // "end line" not "end 1" 
+
+    return 0; // no errors
+}
