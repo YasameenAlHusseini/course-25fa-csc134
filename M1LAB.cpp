98568fd0243d3353206747185126ab493193a8b2
Yasameen Al-Husseini <yasameenalhusseini@gmail.com>
2025-09-16 04:27:56 +0000

    :(

--- M1LAB.cpp
+++ M1LAB.cpp
@@ -6,24 +6,24 @@
 #include <iostream>
 using namespace std;
 int main() {
-// This program will simulate an apple orchard.
-// The owner’s name
-string name = “Yasameen Al-Husseini”
-// number of apples owned
-int apples = 100
-// price per apple
-double pricePerApple = 0.25
-// calculate the total price of the apples
-// print all the information about the orchard
-cout << "Welcome to " << name;
-cout << " ’s apple orchard." << endl;
-cout << "We have " << apples;
-cout << " apples in stock" << endl;
-cout << "Apples are currently $";
-cout << pricePerApple << " each." << endl;
-// print all the information about the orchard
-// calculate the total price of the apples
-double totalPrice = apples * pricePerApple;
-cout << “Welcome to “ << name;
-cout << “ ’s apple orchard.” << endl;
+    // This program will simulate an apple orchard.
+    // The owner’s name
+    string name = "Yasameen Al-Husseini"
+    // number of apples owned
+    int apples = 255
+    // price per apple
+    double pricePerApple = 0.25
+    // calculate the total price of the apples
+    // print all the information about the orchard
+    cout << "Welcome to " << name;
+    cout << " ’s apple orchard." << endl;
+    cout << "We have " << apples;
+    cout << " apples in stock" << endl;
+    cout << "Apples are currently $";
+    cout << pricePerApple << " each." << endl;
+    // print all the information about the orchard
+    // calculate the total price of the apples
+    double totalPrice = apples * pricePerApple;
+    cout << "Welcome to " << name;
+    cout << " 's apple orchard." << endl;
 }
