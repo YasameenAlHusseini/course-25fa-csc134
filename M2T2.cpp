613e1748619ea31b901ebb955db1317fdc492320
Yasameen Al-Husseini <yasameenalhusseini@gmail.com>
2025-09-29 02:23:03 +0000

    halfway

---
+++ M2T2.cpp
@@ -0,0 +1,36 @@
+/*
+CSC 134
+M2T2 - Receipt Calculator
+Yasameen Al-Husseini
+9/28/25
+*/
+
+#include <iostream>
+#include <iomanip> // for 2 descimal places
+using namespace std;
+
+int main() {
+    // Purpose - Create a simple recipt, also handle slaes taxe (8%)
+  
+    // Declare out variables
+  string item = "🍔 Hamburger";
+    double item_price = 5.99;
+    double tax_percent = 0.88; // 8% is 8/100
+    double tax_amount;        // tax in $
+    double total;             // price + tax
+
+    // Greet the user and take the order
+    cout << "Welcome to out CSC 134 Restaurant!" << endl;
+    cout << "You ordered one " << item << "." << endl;
+
+    // Claculate the meal price 
+    // Calculate the sales tax and the total price
+    tax_amount = item_price * tax_percent; // 8% of the item
+    total = item_price +tax_amount;
+
+    // Print the recipt
+    cout << setprecision(2) << fixed;
+    cout << total << endl;
+
+    return 0; // no errors
+}
